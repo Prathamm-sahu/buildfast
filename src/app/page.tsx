d4ec898344a55617c6c26c99c4573745fe080e6f
Pratham Sahu <prathamsahu051@gmail.com>
2025-02-06 13:27:27 +0530

    import components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+import AboutSection from "@/components/AboutSection";
+import Events from "@/components/Events";
+import Footer from "@/components/Footer";
+import Gallery from "@/components/Gallery";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
+import Newsletter from "@/components/Newsletter";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#f5e6d3]">
       <Navbar />
       <HeroSection />
+      <AboutSection />
+      <Events />
+      <Gallery />
+      <Newsletter />
+      <Footer />
     </div>
   );
 }
