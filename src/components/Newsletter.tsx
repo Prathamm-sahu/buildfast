a6b3d203b7ed83a18461c95bb1076b01c1571e32
Pratham Sahu <prathamsahu051@gmail.com>
2025-02-08 03:00:50 +0530

    updated newsletter

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,17 +1,19 @@
-import { FC } from 'react'
-import Image from 'next/image'
-import { Button } from './ui/button'
-import { Input } from './ui/input'
+"use client";
 
-interface NewsletterProps {
-  
-}
+import { FC, useState } from "react";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+interface NewsletterProps {}
 
 const Newsletter: FC<NewsletterProps> = ({}) => {
+  const [email, setEmail] = useState("");
   return (
-    <section className="relative h-[400px] overflow-hidden">
+    <div className="container mx-auto px-14 rounded-lg">
+      <section className="relative h-[500px] overflow-hidden rounded-lg">
         <Image
-          src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-4DYMmrtDSBQY5hVKnwxLqISE92wZqU.png"
+          src="/newsletter.jpg"
           alt="Newsletter Background"
           fill
           className="object-cover"
@@ -20,14 +22,29 @@ const Newsletter: FC<NewsletterProps> = ({}) => {
         <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
           <h2 className="text-4xl font-serif mb-2">Newsletter</h2>
           <h3 className="text-3xl font-serif mb-4">Stay Updated with ISS</h3>
-          <p className="mb-8 text-white/90">Lorem ipsum dolor sit amet consectetur. Et viverra ut odio vulputate.</p>
-          <div className="flex w-full max-w-md gap-4 px-4">
-            <Input type="email" placeholder="Email" className="bg-white" />
-            <Button className="bg-[#4A321D] text-white hover:bg-[#4A321D]/90">Subscribe</Button>
+          <p className="mb-8 text-white/90">
+            Lorem ipsum dolor sit amet consectetur. Et viverra ut odio
+            vulputate.
+          </p>
+          <div className="flex w-full max-w-md px-4">
+            <Input
+              type="email"
+              value={email}
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              className="bg-white text-[#4A321D] rounded-xl w-96 rounded-r-none"
+            />
+            <Button
+              onClick={() => setEmail("")}
+              className="bg-[#4A321D] text-white hover:bg-[#4A321D]/90 w-48 rounded-xl rounded-l-none "
+            >
+              Subscribe
+            </Button>
           </div>
         </div>
       </section>
-  )
-}
+    </div>
+  );
+};
 
-export default Newsletter
+export default Newsletter;
